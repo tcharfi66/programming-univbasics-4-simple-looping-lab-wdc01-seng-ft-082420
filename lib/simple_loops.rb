10729cf318e0e3d2a71e26ffc98601ea0371036b
Tej Charfi <charfi@usc.edu>
2020-08-16 16:16:09 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -3,9 +3,31 @@
 
 def loop_message_five_times(message)
   ctr = 0 
-  
   while ctr < 5 do
     puts message
-    ctr += 1
+    ctr+= 1
   end
+end
+
+
+def loop_message_n_times(message,n)
+  ctr = 0
+  while ctr < n do 
+    puts messagectr+= 1
+  end
+end
+
+def output_array(message, n)
+  
+end
+
+def return_string_array(arr)
+  new_Arr = []
+  l= arr.length()
+  ctr
+  while l > 0 
+    new_Arr[ctr] = arr[ctr].to_s
+    ctr++
+  end
+  
 end
