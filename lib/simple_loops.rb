79962be06d8adc1cf567d0d60f4f94b78c759a11
Tej Charfi <charfi@usc.edu>
2020-08-16 16:27:41 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -11,27 +11,22 @@ end
 
 
 def loop_message_n_times(message,n)
-  
   ctr = 0
   while ctr < n do 
     puts message
     ctr+= 1
   end
-  
 end
 
 def output_array(message)
-  
   ctr = 0
   while ctr < message.length do 
     puts message[ctr]
     ctr+= 1
   end
-  
 end
 
 def return_string_array(array)
-  
   ctr= 0 
   new_arr = []
   while ctr < array.length do 
